b1b29b88bc152a6032b365ddbc9339e4f4973db7
Pete Colley <pc386@kent.ac.uk>
2018-03-22 14:32:36 +0000

    updating

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default class SaveButton extends React.Component {
-	render() {
-		var button = 	this.props.saved ? 		{class: 'sv-alert-success', label:'Saved', onClick: () => {this.props.delete()}, disabled: typeof(this.props.delete) !== 'function' } :
-									this.props.loading ? 	{class: 'sv-alert-warning', label:'Saving', onClick:() => {}, disabled:true } :
-																				{class: 'sv-alert-danger', label:'Save', onClick:() => {this.props.save()}, disabled: typeof(this.props.save) !== 'function' }
+    render() {
+        var button = this.props.saved ? { class: 'sv-btn-success', label: 'Saved', onClick: () => { this.props.delete() }, disabled: typeof(this.props.delete) !== 'function' } :
+            this.props.loading ? { class: 'sv-btn-warning', label: 'Saving', onClick: () => {}, disabled: true } : { class: 'sv-btn-danger', label: 'Save', onClick: () => { this.props.save() }, disabled: typeof(this.props.save) !== 'function' }
 
-		return <button onClick={button.onClick} className={'sv-btn sv-btn-block ' + button.class} type="button" disabled={button.disabled}>{button.label}</button>
-	}
+        return <button onClick = { button.onClick }
+        className = { 'sv-btn sv-btn-block ' + button.class }
+        type = "button"
+        disabled = { button.disabled } > { button.label } </button>
+    }
 }
--- src/util/retrieveState.js
+++ src/util/retrieveState.js
@@ -1,8 +1,8 @@
-export default function () {
-	if (document.querySelector('[data-state]') && document.querySelector('[data-state]').value !== '') {
-		return document.querySelector('[data-state]').value;
-	}
-	else {
-		return false;
-	}
+export default function() {
+    if (document.querySelector('[data-state]') && document.querySelector('[data-state]').value !== '') {
+        console.log('retrieving', document.querySelector('[data-state]').value)
+        return document.querySelector('[data-state]').value;
+    } else {
+        return false;
+    }
 }
